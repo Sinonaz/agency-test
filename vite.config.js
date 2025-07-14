6391d9fb3e3aef375996071923b0bbf8410cf9c7
Isaac <aziksimpson@gmail.com>
2025-07-14 23:00:33 +0200

    Править путь для деплоя

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ const BLD_PATH = './build'
 
 export default defineConfig({
   root: resolve(__dirname, SRC_PATH),
-  base: '/',
+  base: './',
   build: {
     emptyOutDir: true,
     outDir: resolve(__dirname, BLD_PATH),
