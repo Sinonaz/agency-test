6261d44be928eb309fc8d6babf0892b2b3a69a0a
Isaac <aziksimpson@gmail.com>
2025-07-14 22:41:18 +0200

    Поправляет адаптивность на мобильной версии

--- src/javascript/catalog/products/template.js
+++ src/javascript/catalog/products/template.js
@@ -6,11 +6,16 @@ export const getProductTemplate = ({ name, price, id, isAvailable, isSale, image
 
           <div class="product__content">
             <a href="#" class="product__name">${name}</a>
-            <p class="product__price">${price} ₽</p>
-            <button class="product__btn js-add-cart" data-open-target="js-cart-modal" data-id="${id}" type="button" aria-label="Добавить в корзину" ${!isAvailable ? 'disabled' : ''}>
-              <svg width="20" height="20" viewBox="0 0 20 20">
-                <use xlink:href="/img/sprite.svg#plus"></use>
-              </svg>
+            <p class="product__price">${price}&nbsp;₽</p>
+            <button class="product__btn js-add-cart" data-open-target="js-cart-modal" data-id="${id}" type="button" ${!isAvailable ? 'disabled' : 'aria-label="Добавить в корзину"'}>
+            ${
+              !isAvailable
+                ? 'Недоступно'
+                : '<svg width="20" height="20" viewBox="0 0 20 20">\n' +
+                  '                <use' +
+                  ' xlink:href="/img/sprite.svg#plus"></use>\n              </svg>'
+            }
+
             </button>
           </div>
         </article>`
